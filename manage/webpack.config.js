dd1927340a77f4529a40f8848551314dd7abf188
quancongjin550 <quancongjin55@pingan.com.cn>
2018-02-26 11:03:28 +0800

    update

--- manage/webpack.config.js
+++ manage/webpack.config.js
@@ -5,7 +5,7 @@ const path = require('path');
 const shell = require('shelljs')
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin'); //css单独打包
-const CompressionPlugin = require('compression-webpack-plugin');//资源文件压缩为.gz文件
+const CompressionPlugin = require('compression-webpack-plugin'); //资源文件压缩为.gz文件
 const root = path.resolve(__dirname, './') // 项目的根目录绝对路径
 
 
@@ -26,7 +26,7 @@ module.exports = {
     context: path.resolve(__dirname + '/static/js/'),
     entry: {
         index: './index.js',
-        vue:['vue', 'vue-router', 'vuex','axios'],
+        vue: ['vue', 'vue-router', 'vuex', 'axios'],
         elementui: 'element-ui'
     },
     output: {
@@ -69,13 +69,21 @@ module.exports = {
         //     filename: '[name].[hash].js',
         //     chunks: ['index', 'common']    // extract commonChunk  from index & common
         // }),
-        new webpack.optimize.CommonsChunkPlugin({name:['vue','elementui'],filename: '[name].js'}),
+        new webpack.optimize.CommonsChunkPlugin({
+            name: ['vue', 'elementui'],
+            filename: '[name].js'
+        }),
         new webpack.DefinePlugin({
          //    // 开发环境是不是生产
             PRODUCTION: JSON.stringify(false),
         }),
+        // new webpack.DefinePlugin({
+        //     'process.env': {
+        //         NODE_ENV: JSON.stringify('develop')
+        //     }
+        // }),
         new HtmlWebpackPlugin({  //根据模板插入css/js等生成最终HTML
-            title: '测试',// 模板参数
+            title: '测试', // 模板参数
             // msg: '这是一个测试参数的测试参数',// 模板参数
             filename: '../index.html', //生成的html存放路径，相对于 output -> path
             template: '../template/index.html', //html模板路径, 相对于 output -> path
@@ -89,57 +97,88 @@ module.exports = {
         extensions: ['.js', '.vue', '.css'],
         alias: {
             'vue$': 'vue/dist/vue.common.js'
+            // 'vue$': 'vue/dist/vue.js'
+            // 'vue$': 'vue/dist/vue.esm.js'
         }
     },
     module: {
-        loaders: [
-            {
-                test: /\.js$/,
-                //include: '', //要处理的目录
-                exclude: /node_modules/,//排除不处理的目录
-                loader: 'babel-loader',
-                query:{
-                    // .babelrc 存在是   优先.babelrc
-                    presets:['es2015'/*['es2015', {
+        loaders: [{
+            test: /\.js$/,
+            //include: '', //要处理的目录
+            exclude: /node_modules/, //排除不处理的目录
+            loader: 'babel-loader',
+            query: {
+                // .babelrc 存在是   优先.babelrc
+                presets: ['es2015'
+                    /*['es2015', {
                         "modules": false  //消除unused code
-                      }]*/],
-                    // 解决编译打包后 $export is not a function  异常
-                    plugins: [['transform-runtime', {
-                              helpers: false,
-                              polyfill: false,
-                              regenerator: true, }],
-                              /*[// 如果你的项目中用到了UI组件库，只加载用到的组件
+                      }]*/
+                ],
+                // 解决编译打包后 $export is not a function  异常
+                plugins: [
+                    ['transform-runtime', {
+                        helpers: false,
+                        polyfill: false,
+                        regenerator: true,
+                    }],
+                    /*[// 如果你的项目中用到了UI组件库，只加载用到的组件
                                "import", {  //babel-plugin-import
                                     "libraryName": "element-ui",
                                     "style": true
                                }],*/
-                    ]
-                }
-            },{
-                test: /\.vue$/,
-                exclude: /^node_modules$/,//排除不处理的目录
-                loader: 'vue-loader'
-            }, {
-                test: /\.css$/,
-                loader: ExtractTextPlugin.extract({fallback:'style-loader',use:'css-loader'})
-                // use: ExtractTextPlugin.extract({fallback:'style-loader',use:['css-loader']})
-            }, {
-                test: /\.scss$/,
-                loader: ExtractTextPlugin.extract({fallback:'style-loader',use:['css-loader', 'sass-loader']})
-            }, {
-                test: /\.(eot|svg|ttf|woff|woff2)(\?\S*)?$/,
-                loader: 'file-loader',
-                query: {
-                    name: '../incofont/[name].[hash:8].[ext]'
-                }
-            }, {
-                test: /\.(png|jpe?g|gif|svg)(\?\S*)?$/,
-                loader: 'file-loader',
-                query: {
-                    limit: 8192,
-                    name: '../images/[name].[hash:8].[ext]'
-                }
+                ]
             }
-        ]
+        }, {
+            test: /\.vue$/,
+            exclude: /^node_modules$/, //排除不处理的目录
+            loader: 'vue-loader'
+        }, {
+            test: /\.css$/,
+            loader: ExtractTextPlugin.extract({
+                fallback: 'style-loader',
+                use: 'css-loader'
+            })
+            // use: ExtractTextPlugin.extract({fallback:'style-loader',use:['css-loader']})
+        }, {
+            test: /\.scss$/,
+            loader: ExtractTextPlugin.extract({
+                fallback: 'style-loader',
+                use: ['css-loader', 'sass-loader']
+            })
+        }, {
+            test: /\.(eot|svg|ttf|woff|woff2)(\?\S*)?$/,
+            loader: 'file-loader',
+            query: {
+                name: '../incofont/[name].[hash:8].[ext]'
+            }
+        }, {
+            test: /\.(png|jpe?g|gif|svg)(\?\S*)?$/,
+            loader: 'file-loader',
+            query: {
+                limit: 8192,
+                name: '../images/[name].[hash:8].[ext]'
+            }
+        }]
     }
 };
+
+
+if (process.env.NODE_ENV === 'production') {
+    module.exports.devtool = '#source-map'
+    // http://vue-loader.vuejs.org/en/workflow/production.html
+    module.exports.plugins = (module.exports.plugins || []).concat([
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: '"production"'
+            }
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false
+            }
+        }),
+        new webpack.LoaderOptionsPlugin({
+            minimize: true
+        })
+    ])
+}
