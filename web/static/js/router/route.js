70073283d01825e45b1bdfba4b0dd873dcdec308
qualc <493549174@qq.com>
2018-03-22 22:22:12 +0800

    update

--- web/static/js/router/route.js
+++ web/static/js/router/route.js
@@ -6,21 +6,21 @@ default [{
 },{
     path: '/article/:type?',
     name: 'article',
-    component: require('../views/article/sfArticle.vue')
+    component: resolve => require(['../views/article/sfArticle.vue'], resolve)
 }, {
     path: '/articleinfo/:id',
     name: 'articleinfo',
-    component: require('../views/article/sfArticleInfo.vue')
+    component: resolve => require(['../views/article/sfArticleInfo.vue'], resolve)
 }, {
     path: '/articleclassify/:type',
     name: 'articleclassify',
-    component: require('../views/article/sfArticleClassify.vue')
+    component: resolve => require(['../views/article/sfArticleClassify.vue'], resolve)
 }, {
     path: '/articlelist/:type/:id',
     name: 'articlelist',
-    component: require('../views/article/sfArticleList.vue')
+    component: resolve => require(['../views/article/sfArticleList.vue'], resolve)
 }, {
     path: '/authorlist',
     name: 'authorlist',
-    component: require('../views/author/sfAuthorList.vue')
+    component: resolve => require(['../views/author/sfAuthorList.vue'], resolve)
 }]
